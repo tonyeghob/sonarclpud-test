472bf9669c5a9b761c0fed73dc0904d858fab873
Anthony Eghobor <lonag36@LONAG36-M1.local>
2018-12-21 12:41:33 +0000

    added the new files

--- index.js
+++ index.js
@@ -17,10 +17,10 @@ app.use(express.static(path.join(__dirname, 'static')));
 
 //create mysql Connection
   var connection = mysql.createConnection({
-  	//host: 'localhost',
-  	//user: 'node1',
-  	//password: 'node',
-  	//database: 'node_project'
+//  	host: 'localhost',
+//  	user: 'node1',
+//  	password: 'node',
+//  	database: 'node_project'
 
   	user: keys.mysqlUser,
   	host: keys.mysqlHost,
@@ -47,11 +47,19 @@ app.use(express.static(path.join(__dirname, 'static')));
 //app.set('views', './views');
 
 app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname+ '/Arcus.html'));
+  res.sendFile(path.join(__dirname+ '/index.html'));
 });
 
-app.get('/form', function(req, res) {
-  res.sendFile(path.join(__dirname+ '/ArcusForm.html'));
+app.get('/index.html', function(req, res) {
+  res.sendFile(path.join(__dirname+ '/index.html'));
+});
+
+app.get('/add_project', function(req, res) {
+  res.sendFile(path.join(__dirname+ '/add_project.html'));
+});
+
+app.get('/help', function(req, res) {
+  res.sendFile(path.join(__dirname+ '/help.html'));
 });
 
 // for parsing application/json
@@ -65,16 +73,26 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(upload.array());
 app.use(express.static('public'));
 
-app.post('/postaction', function(req, res) {
+
+
+//app.post('/post_project', function(req, res) {
+//  res.send('Thanks for submitting data about your ' + req.body.family + ' project.');
+
+//  console.log(req.body.family);
+//});
+
+
+app.post('/post_project', function(req, res) {
   res.send('Thanks for submitting data about your ' + req.body.family + ' project.');
 
-var formData = [[req.body.family,req.body.product,req.body.experience,req.body.client,req.body.contact]];
+
+var formData = [[req.body.family,req.body.product,req.body.rating,req.body.client,req.body.contact_name,req.body.overview,req.body.provider,req.body.project,req.body.start_date,req.body.end_date,req.body.status,req.body.contact_email]];
 
 console.log(formData);
 
   //});
-connection.query('CREATE TABLE IF NOT EXISTS arcus (family varchar(250) DEFAULT NULL, product varchar(320) DEFAULT NULL, experience text(65532) DEFAULT NULL,client varchar(320) DEFAULT NULL,contact varchar(320) DEFAULT NULL );')
-connection.query('INSERT INTO arcus (family, product, experience, client, contact) VALUES ?', [formData]) , function(req,res,err) {
+connection.query('CREATE TABLE IF NOT EXISTS arcus (family varchar(250) DEFAULT NULL, product varchar(320) DEFAULT NULL,  experience int(255) DEFAULT NULL, client varchar(320) DEFAULT NULL,contact varchar(320) DEFAULT NULL,overview text(65532) DEFAULT NULL,provider varchar(100) DEFAULT NULL,project varchar(320) DEFAULT NULL, start_date varchar(100) DEFAULT NULL,end_date varchar(320) DEFAULT NULL,status varchar(100) DEFAULT NULL,contact_email varchar(320) DEFAULT NULL );')
+connection.query('INSERT INTO arcus (family, product,experience, client,contact,overview,provider,project,start_date,end_date,status,contact_email) VALUES ?', [formData]) , function(req,res,err) {
   if(err) {
     res.send('Error');
     }
@@ -97,10 +115,16 @@ app.get('/', function(req, res) {
 //var searchData = [[req.query]];
 console.log(req.query.search);
 
-    connection.query("SELECT * FROM arcus WHERE family  like '%" + req.query.search + "%'", function(err, rows, fields) {
-         res.render('search',{data:rows});
+    //connection.query("SELECT * FROM arcus WHERE family  like '%" + req.query.search + "%'", function(err, rows, fields) {
+    connection.query("SELECT * FROM arcus WHERE CONCAT(IFNULL(family, ''),IFNULL(product, ''),IFNULL(experience, ''),IFNULL(client, ''),IFNULL(contact, ''),IFNULL(overview, ''),IFNULL(provider,''),IFNULL (project, ''),IFNULL(status, ''))  like '%" + req.query.search + "%'", function(err, rows, fields) {
+console.log(rows);
+         res.render('search_results',{data:rows});
 
      });
 
 
 });
+
+
+
+//SELECT * FROM clients WHERE CONCAT(field1, '', field2, '', fieldn) LIKE "%Mary%"
